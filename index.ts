7e07421f5c73e2aaa09056823329aea48f7bca0c
Kunal-Ch-Das-Official <kunalchandradasofficial@gmail.com>
2026-08-13 09:33:37 +0530

    change name subatom -> package

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-export * from "./subatom/core/bootstrap/subatom/Subatom.js";
-export * from "./subatom/core/router/Router.js";
-export { configEnv, env } from "./subatom/cli-engine/utils/env/env.js";
-export { parseEnv } from "./subatom/cli-engine/utils/env/parseEnv.js";
+export * from "./package/core/bootstrap/subatom/Subatom.js";
+export * from "./package/core/router/Router.js";
+export { configEnv, env } from "./package/cli-engine/utils/env/env.js";
+export { parseEnv } from "./package/cli-engine/utils/env/parseEnv.js";
 
 // --- Middleware Functions (Individual Named Exports) ---
 import {
   array,
   fields,
   single,
-} from "./subatom/core/pipeline/file-system/fileUploadPipe.js";
+} from "./package/core/pipeline/file-system/fileUploadPipe.js";
 
 export const file = {
   single,
@@ -17,56 +17,56 @@ export const file = {
 };
 
 // Export config
-export { defineConfig } from "./subatom/config/load.config.js";
-export { UploadFile } from "./subatom/core/pipeline/file-system/UploadFile.js";
+export { defineConfig } from "./package/config/load.config.js";
+export { UploadFile } from "./package/core/pipeline/file-system/UploadFile.js";
 
-export { cors } from "./subatom/core/securities/cross-origin/index.js";
-export * from "./subatom/core/securities/cross-origin/index.js";
-export { json } from "./subatom/core/factory-functions/json.js";
-export { raw } from "./subatom/core/factory-functions/raw.js";
-export { serveStatic } from "./subatom/core/factory-functions/serveStatic.js";
-export { text } from "./subatom/core/factory-functions/text.js";
-export { urlencoded } from "./subatom/core/factory-functions/urlencoded.js";
-export { session } from "./subatom/core/factory-functions/session.js";
+export { cors } from "./package/core/securities/cross-origin/index.js";
+export * from "./package/core/securities/cross-origin/index.js";
+export { json } from "./package/core/factory-functions/json.js";
+export { raw } from "./package/core/factory-functions/raw.js";
+export { serveStatic } from "./package/core/factory-functions/serveStatic.js";
+export { text } from "./package/core/factory-functions/text.js";
+export { urlencoded } from "./package/core/factory-functions/urlencoded.js";
+export { session } from "./package/core/factory-functions/session.js";
 
 //Types
 export type {
   SubatomConfig,
   SubatomUserConfig,
-} from "./subatom/types/config/SubatomConfig.js";
+} from "./package/types/config/SubatomConfig.js";
 
 export type {
   IRequest,
   RequestOptions,
   RequestFiles,
-} from "./subatom/types/http/IRequest.js";
+} from "./package/types/http/IRequest.js";
 export type {
   IResponse,
   CookieOptions,
   SendFileOptions,
   DownloadOptions,
-} from "./subatom/types/http/IResponse.js";
+} from "./package/types/http/IResponse.js";
 
 export type {
   NextFunction,
   INext,
-} from "./subatom/types/framework/pipeline/INext.js";
+} from "./package/types/framework/pipeline/INext.js";
 
 export type {
   UploadFileOptions,
   Middleware,
   IUploadFile,
   FilesMap,
-} from "./subatom/types/framework/pipeline/IUploadFile.js";
+} from "./package/types/framework/pipeline/IUploadFile.js";
 
 // export streams with types
-export * from "./subatom/core/http/streams/index.js";
-export * from "./subatom/core/websocket/sse/index.js";
+export * from "./package/core/http/streams/index.js";
+export * from "./package/core/websocket/sse/index.js";
 
 //
-export * from "./subatom/core/http/compression/index.js";
-export * from "./subatom/core/http/compression//staticPrecompress.js";
+export * from "./package/core/http/compression/index.js";
+export * from "./package/core/http/compression/staticPrecompress.js";
 
 // Security
-export * from "./subatom/core/securities/security/index.js";
-export * from "./subatom/core/securities/rate-limits/index.js";
+export * from "./package/core/securities/security/index.js";
+export * from "./package/core/securities/rate-limits/index.js";
